cc6b0ec22a8004304d74911f0d5be21cfdc883f5
SATHVIK SVS <svssathvik77@gmail.com>
2024-08-20 11:19:19 +0530

    created a pass key status utility controller

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { authUser, registerUser, validateToken } from '../controllers/authController.js'
+import { authUser, registerUser, validateToken, codeStatusUtil } from '../controllers/authController.js'
 
 const router = express.Router()
 
 router.post('/login', authUser)
 router.post('/register', registerUser)
 router.post('/validate_token', validateToken)
-
+router.get("/code-validity",codeStatusUtil);
 export default router;
